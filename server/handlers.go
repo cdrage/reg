8c8373bd05779962307c261826f5a0af9f708991
Bamacharan Kundu <bamachrn@gmail.com>
2019-01-24 09:40:28 +0530

    Remove old landing page renderer and dockerfile retrieval

--- server/handlers.go
+++ server/handlers.go
@@ -112,7 +112,7 @@ type TagDetails struct {
 }
 
 func getAPIData(uri string, datatype string) (interface{}, error) {
-	url := APIURL + "/v1/namespaces/pipeline/" + uri
+	url := APIURL + "/v1/namespaces/" + NAMESPACE + "/" + uri
 
 	var DataType map[string]interface{}
 	DataType = make(map[string]interface{})
@@ -147,109 +147,6 @@ func (rc *registryController) landingPageHandler(w http.ResponseWriter, r *http.
 		"method": r.Method,
 	}).Info("Landing Page")
 	http.Redirect(w, r, "/containers/", http.StatusSeeOther)
-}
-
-// Download Dockerfiles to the server
-// Puts it into a specific directory.
-
-func (rc *registryController) dockerfiles(dockerfilesDir string) error {
-	/*
-		// Retrieve the catalog
-		logrus.Info("fetching catalog for dockerfile retrieval")
-
-		result := AnalysisResult{
-			RegistryDomain: rc.reg.Domain,
-			LastUpdated:    time.Now().Local().Format(time.RFC1123),
-		}
-
-		repoList, err := r.Catalog("")
-		if err != nil {
-			return fmt.Errorf("getting catalog failed: %v", err)
-		}
-
-		for _, repo := range repoList {
-			repoURI := fmt.Sprintf("%s/%s", rc.reg.Domain, repo)
-			r := Repository{
-				Name: repo,
-				URI:  repoURI,
-			}
-			result.Repositories = append(result.Repositories, r)
-		}
-
-		// Retrieve all Dockerfiles :)
-		logrus.Info("retrieving Dockerfiles (http get)")
-
-		// TODO: Instead of grabbing the *first* id in the cccp.yaml file, possible *show* Dockerfile from each tag instead in the HTML?
-		repos, err := retrieveIndex()
-		if err != nil {
-			return fmt.Errorf("getting dockerfiles from index failed: %v", err)
-		}
-
-		for _, repo := range repos {
-
-			// TODO: A bit "hacky" but this will do for now.
-			namespace := repo.Projects[0].AppID
-
-			for _, c := range repo.Projects {
-
-				// TODO: A bit "hacky" but this will do for now.
-				container := c.JobID
-				dockerfileContents := c.Dockerfile
-				readme := c.Readme
-
-				dockerfilePath := path.Join(dockerfilesDir, namespace, container)
-
-				// Create the folder if it's not already there
-				logrus.Debugf("creating dir %s", dockerfilePath)
-				err = os.MkdirAll(dockerfilePath, os.ModePerm)
-				if err != nil {
-					return err
-				}
-
-				// Create / open the file
-				logrus.Debugf("creating/opening file %s", path.Join(dockerfilePath, "Dockerfile"))
-				f, err := os.Create(path.Join(dockerfilePath, "Dockerfile"))
-				defer f.Close()
-				if err != nil {
-					return err
-				}
-
-				// Write Dockerfile contents to file if Dockerfile != ""
-				if dockerfileContents != "" {
-					_, err = io.Copy(f, strings.NewReader(dockerfileContents))
-					if err != nil {
-						return errors.Wrap(err, "Unable to write to Dockerfile")
-					}
-				}
-
-				// Create / open the file
-				logrus.Debugf("creating/opening file %s", path.Join(dockerfilePath, "README.md"))
-				readmeContents, err := os.Create(path.Join(dockerfilePath, "README.md"))
-				defer readmeContents.Close()
-				if err != nil {
-					return err
-				}
-
-				// Write README contents to file if Readme != ""
-				if readme != "" {
-					// Convert the readme to valid (safe) HTML
-					unsafe := blackfriday.Run([]byte(readme))
-					html := bluemonday.UGCPolicy().SanitizeBytes(unsafe)
-
-					_, err = io.Copy(readmeContents, strings.NewReader(string(html)))
-					if err != nil {
-						return errors.Wrap(err, "Unable to write to Readme")
-					}
-				}
-
-				if err != nil {
-					return err
-				}
-
-			}
-		}
-	*/
-	return nil
 }
 
 //This function renders the landing page of registry
@@ -292,66 +189,6 @@ func (rc *registryController) imageListHandler(w http.ResponseWriter, r *http.Re
 	}
 	return
 }
-
-/*
-	updating = true
-	logrus.Info("fetching catalog")
-
-	result := AnalysisResult{
-		RegistryDomain: rc.reg.Domain,
-		LastUpdated:    time.Now().Local().Format(time.RFC1123),
-	}
-
-	repoList, err := r.Catalog("")
-	if err != nil {
-		return fmt.Errorf("getting catalog failed: %v", err)
-	}
-
-	for _, repo := range repoList {
-		repoURI := fmt.Sprintf("%s/%s", rc.reg.Domain, repo)
-
-		// Retrieve number of tags
-		tags, err := rc.reg.Tags(repo)
-		if err != nil {
-			logrus.Warningf("Ignoring user %s, unable to retrieve tags: %v.", repo, err)
-		}
-
-		// If there are actually tags, we add this to the main site, otherwise, we ignore it.
-		if len(tags) != 0 {
-			r := Repository{
-				Name: repo,
-				URI:  repoURI,
-				Tags: len(tags),
-			}
-			result.Repositories = append(result.Repositories, r)
-		}
-
-	}
-
-	// parse & execute the template
-	logrus.Info("executing the template containers")
-
-	// Use /repo/index.html instead
-	path := filepath.Join(staticDir, "/containers/index.html")
-	if err := os.MkdirAll(filepath.Dir(path), 0644); err != nil {
-		return err
-	}
-	logrus.Debugf("creating/opening file %s", path)
-	f, err := os.Create(path)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	if err := tmpl.ExecuteTemplate(f, "containers", result); err != nil {
-		f.Close()
-		return fmt.Errorf("execute template containers failed: %v", err)
-	}
-
-	updating = false
-	return nil
-*/
-//}
 
 func (rc *registryController) tagListHandler(w http.ResponseWriter, r *http.Request) {
 	/*	logrus.WithFields(logrus.Fields{
